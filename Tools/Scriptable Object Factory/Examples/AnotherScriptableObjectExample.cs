64813e316bb5e006cf8ebd067398bd6ea1cc7492
Bodix <bodix321@gmail.com>
2025-06-18 11:20:13 +0300

    Cleanup unnecessary scripts. Add autogenerated meta files

--- Tools/Scriptable Object Factory/Examples/AnotherScriptableObjectExample.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-// Again, there is no need to include a CreateAssetMenu property
-public class AnotherScriptableObjectExample : ScriptableObject
-{
-    public string justSomeString;
-}
